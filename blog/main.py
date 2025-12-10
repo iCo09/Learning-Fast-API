67e8552483505c7f3cdd0f87444897aa56ce06d7
Rahul Bisht <rahul.singh@nestorbird.com>
2025-12-10 23:12:29 +0530

    CRUD using FAST-API

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 models.Base.metadata.create_all(engine)
-
+ 
 
 def get_db():
     db = SessionLocal()
@@ -15,7 +15,7 @@ def get_db():
         db.close( )
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -28,3 +28,27 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def get_by_id(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    return blog 
+
+
+@app.delete('/delete_blog/{id}')
+def destroy_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    db.commit()
+    return "done"
+
+
+@app.put('/update_blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).update(request.dict())
+    db.commit()
+    return 'updated successfully'
